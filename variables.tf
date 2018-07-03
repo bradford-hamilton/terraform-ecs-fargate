9dfd57d9fb35754a8d434d0b591d4d656b022506
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-07-03 14:43:09 -0600

    Add terraform variables

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-2"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "bradfordhamilton/crystal_blockchain:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
