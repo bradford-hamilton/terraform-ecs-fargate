bf8cb79e2bc3130766d02d245e4f08c42a459c83
borgified <borgified@gmail.com>
2019-10-09 11:29:44 -0700

    remove role from aws_appautoscaling_target
    fixes idempotency issue with the role being replaced by AWS's service linked
    role. (see https://github.com/azavea/terraform-aws-ecs-web-service/pull/13)

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -24,29 +24,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-# ECS auto scale role data
-data "aws_iam_policy_document" "ecs_auto_scale_role" {
-  version = "2012-10-17"
-  statement {
-    effect = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["application-autoscaling.amazonaws.com"]
-    }
-  }
-}
-
-# ECS auto scale role
-resource "aws_iam_role" "ecs_auto_scale_role" {
-  name               = var.ecs_auto_scale_role_name
-  assume_role_policy = data.aws_iam_policy_document.ecs_auto_scale_role.json
-}
-
-# ECS auto scale role policy attachment
-resource "aws_iam_role_policy_attachment" "ecs_auto_scale_role" {
-  role       = aws_iam_role.ecs_auto_scale_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
-}
