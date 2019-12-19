815184abd6fd84c5df8b29e0bc187c54ae02fe98
Brandon Hilkert <brandonhilkert@gmail.com>
2019-12-18 22:03:24 -0800

    Fix upper bound on scale down action
    I found this setting to cause the scale down action to file without any error text. I stumbled on [this post](https://superuser.com/a/1508956) that had the suggestion. After making the change, the service scaled down immediately in the alarm state.

--- terraform/auto_scaling.tf
+++ terraform/auto_scaling.tf
@@ -42,7 +42,7 @@ resource "aws_appautoscaling_policy" "down" {
     metric_aggregation_type = "Maximum"
 
     step_adjustment {
-      metric_interval_lower_bound = 0
+      metric_interval_upper_bound = 0
       scaling_adjustment          = -1
     }
   }
